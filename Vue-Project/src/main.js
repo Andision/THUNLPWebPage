22b16160cda0ac7fd919ab0b559a02c6a4fc1d6e
Andision <andision@outlook.com>
2021-05-18 15:30:26 +0800

    Add Vue component

--- Vue-Project/src/main.js
+++ Vue-Project/src/main.js
@@ -3,8 +3,47 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(ElementUI, {locale})
 
 Vue.config.productionTip = false
+
+Vue.prototype.handleMenuSelect = function (key, keyPath) {
+  console.log(key)
+  // console.log(this.language.Password1)
+  // if (key === '1-1') {
+  //   this.language = zh
+  // } else if (key === '1-2') {
+  //   this.language = en
+  // }
+
+  switch (key) {
+    case '2':
+      router.push({path: '/'})
+      break
+    case '3':
+      router.push({path: '/about'})
+      break
+    case '4-1':
+      router.push({path: '/submit/attack'})
+      break
+    case '5-1':
+      router.push({path: '/leaderboard/attack'})
+      break
+    case '6-1':
+      router.push({path: '/judging'})
+      break
+    case '6-2':
+      router.push({path: '/judging'})
+      break
+    case '7':
+      router.push({path: '/setting'})
+      break
+  }
+}
 
 /* eslint-disable no-new */
 new Vue({
