07e9b10836e4c013f017af1451cdd7ef60b0df5d
Andision <andision@outlook.com>
2021-05-18 23:59:49 +0800

    #1 Finish Login and SignUp (no backend)

--- Vue-Project/src/components/LoginDialog.vue
+++ Vue-Project/src/components/LoginDialog.vue
@@ -1,13 +1,13 @@
 <template>
   <el-dialog :visible.sync="visible" width="500px">
     <el-tabs type="border-card" :stretch="true">
-      <el-tab-pane label="Login">
-        <el-row class="input"> WELCOME TO JOIN US </el-row>
+      <el-tab-pane :label="language.Login" ref="tab_login">
+        <el-row class="input"> {{language.WelcomeToJoinUs}} </el-row>
 
         <el-row class="input">
           <el-input
             v-model="user.username"
-            placeholder="USERNAME"
+            :placeholder="language.Username"
             prefix-icon="el-icon-user"
             size="large"
           ></el-input>
@@ -18,7 +18,7 @@
             type="password"
             v-model="user.password"
             @keydown.enter.native="doLogin"
-            placeholder="PASSWORD"
+            :placeholder="language.Password"
             prefix-icon="el-icon-lock"
             size="large"
           ></el-input>
@@ -26,32 +26,32 @@
         <el-row class="input">
           <el-row>
             <el-col :span="12">
-              <el-checkbox v-model="user.remeber" style="float:left;">REMEBER ME</el-checkbox>
+              <el-checkbox v-model="user.remeber" style="float:left;">{{language.Remeberme}}</el-checkbox>
             </el-col>
             <el-col :span="12">
-              <div style="float:right;">FORGET</div>
+              <div style="float:right;">{{language.Forgetpassword}}?</div>
             </el-col>
           </el-row>
         </el-row>
         <el-row class="input" style="vertical-align: bottom;">
           <el-button style="width: 100%" type="primary" @click="doLogin">
-            LOGIN
+            {{language.Login}}
           </el-button>
         </el-row>
         <el-row class="input" style="vertical-align: bottom;">
           <div>
-            NOT A MEMBER? SIGNUP
+            {{language.Notamember}}? <el-button type="text" @click="handleClickLink(false)">{{language.SignUp}}</el-button>
           </div>
         </el-row>
       </el-tab-pane>
       <!-- --------------------------------- -->
       <el-tab-pane label="Sign Up">
-        <el-row class="input"> WELCOME TO JOIN US </el-row>
+        <el-row class="input"> {{language.WelcomeToJoinUs}} </el-row>
 
         <el-row class="input">
           <el-input
             v-model="user.username"
-            placeholder="USERNAME"
+            :placeholder="language.Username"
             prefix-icon="el-icon-user"
             size="large"
           ></el-input>
@@ -60,7 +60,7 @@
         <el-row class="input">
           <el-input
             v-model="user.email"
-            placeholder="EMAIL"
+            :placeholder="language.Email"
             prefix-icon="el-icon-message"
             size="large"
           ></el-input>
@@ -71,19 +71,19 @@
             type="password"
             v-model="user.password"
             @keydown.enter.native="doLogin"
-            placeholder="PASSWORD"
+            :placeholder="language.Password"
             prefix-icon="el-icon-lock"
             size="large"
           ></el-input>
         </el-row>
         <el-row class="input" style="vertical-align: bottom;">
           <el-button style="width: 100%" type="primary" @click="doLogin">
-            SIGNUP
+            {{language.SignUp}}
           </el-button>
         </el-row>
         <el-row class="input" style="vertical-align: bottom;">
           <div>
-            ALREADY HAVE AN ACCOUNT? LOGIN
+            {{language.Alreadyhaveanaccount}}? <el-button type="text" @click="handleClickLink(true)">{{language.Login}}</el-button>
           </div>
         </el-row>
       </el-tab-pane>
@@ -93,9 +93,11 @@
 <!-- <LoginDoalog v-bind:v="dialogLoginVisible" v-on:close="handleLogin"></LoginDoalog> -->
 <!-- import LoginDoalog from '@/components/LoginDialog.vue' -->
 <script>
+import en from '@/components/en.json'
 export default {
   data () {
     return {
+      language: en,
       visible: this.v,
       user: {
         username: '',
@@ -115,6 +117,15 @@ export default {
     visible (curVal, oldVal) {
       if (!curVal) {
         this.$emit('close', false)
+      }
+    }
+  },
+  methods: {
+    handleClickLink (isLogin) {
+      if (isLogin) {
+        document.getElementById('tab-0').click()
+      } else {
+        document.getElementById('tab-1').click()
       }
     }
   }
