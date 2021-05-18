4e9f47a51c560d183fe1a5a1de0d43c6fdb5f41b
Andision <andision@outlook.com>
2021-05-18 19:06:30 +0800

    #1 Add Login dialog

---
+++ Vue-Project/src/components/LoginDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-dialog :visible.sync="visible" width="500px">
+    <el-tabs type="border-card" :stretch="true">
+      <el-tab-pane label="Login">LOGIN</el-tab-pane>
+      <el-tab-pane label="Sign Up">SIGNUP</el-tab-pane>
+    </el-tabs>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      visible: this.v
+    }
+  },
+  props: {
+    v: Boolean
+  },
+  watch: {
+    v (curVal, oldVal) {
+      this.visible = curVal
+    },
+    visible (curVal, oldVal) {
+      if (!curVal) {
+        this.$emit('close', false)
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
