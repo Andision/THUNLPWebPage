adfe45c12ff3478fccf1858f2cf990c507dc699d
Andision <Andision@outlook.com>
2021-05-24 21:50:28 +0800

    #5 Add axios

--- Vue-Project/src/main.js
+++ Vue-Project/src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
+import '../src/theme/index.css'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
+import axios from 'axios'
+
+Vue.prototype.$axios = axios
 
 Vue.use(ElementUI, {locale})
 
