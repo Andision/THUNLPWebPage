f8c0f05d008f396e21a3cfdcf6d79f2d842718a2
Andision <48558940+Andision@users.noreply.github.com>
2021-07-06 21:15:51 +0800

    Update Home.vue

--- Vue-Project/src/components/Home.vue
+++ Vue-Project/src/components/Home.vue
@@ -113,10 +113,16 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 .el-main{
   background-color: #F5F4FE;
 }
+.main{
+  background-color: #F5F4FE;
+}
+</style>
+
+<style scoped>
 .all{
   background-color: #F5F4FE;
   min-height: 800px;
