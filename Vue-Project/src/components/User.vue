0e8b73c18de12105053a37be7a0e161d82eb40d9
Andision <Andision@outlook.com>
2021-07-23 20:04:54 +0800

    Update User

--- Vue-Project/src/components/User.vue
+++ Vue-Project/src/components/User.vue
@@ -1,170 +1,166 @@
 <template>
   <div class="all">
-    <el-table
-      :data="tableData"
-      class="table"
-      :header-cell-style="{ background: '#64438D', color: 'white' }"
-      @row-click="handleRowClick"
-    >
-      <el-table-column prop="name" :label="language.Name"> </el-table-column>
-      <el-table-column prop="date" :label="language.Date" width="200">
-      </el-table-column>
-      <el-table-column prop="time" :label="language.Time" width="200">
-      </el-table-column>
-      <el-table-column prop="score" :label="language.Score" width="200">
-      </el-table-column>
-    </el-table>
-
-    <el-drawer
-      :visible.sync="drawer"
-      direction="rtl"
-      size="30%"
-      :with-header="false"
-    >
-      <div class="draw">
-        <div class="draw-sub">
-          <h1>#{{drawerInfo.rank}} {{drawerInfo.name}}</h1>
-          <div>
-            {{drawerInfo.link}}
-            Date: {{drawerInfo.date}}
-            <br>
-            Score: {{drawerInfo.score}}
+    <h1 class="title">{{ language.leaderboard_phb }}</h1>
+    <el-card class="main">
+      <div slot="header">
+        <div class="main-title">
+          <div class="title-title">
+            {{ language.task_synl }}
+            <el-switch v-model="showAll" active-color="#64438D"> </el-switch>
           </div>
-        </div>
-        <hr class="draw-hr">
-        <div class="draw-sub">
-          <h1>{{language.Model}}</h1>
-          <div>
-            {{drawerInfo.model}}
+          <hr />
+          <div class="title-sub">
+            <div style="display:flex;flex-direction:row;justify-content: space-between;">
+              <div class="title-sub-section"><div class="title-sub-section-text">{{language.task_sjnl}}</div><div class="title-sub-section-btn"><el-switch v-model="s.sj" active-color="#64438D"></el-switch></div></div>
+              <div class="title-sub-section"><div class="title-sub-section-text">{{language.task_ljnl}}</div><div class="title-sub-section-btn"><el-switch v-model="s.lj" active-color="#64438D"></el-switch></div></div>
+              <div class="title-sub-section"><div class="title-sub-section-text">{{language.task_jsnl}}</div><div class="title-sub-section-btn"><el-switch v-model="s.js" active-color="#64438D"></el-switch></div></div>
+              <div class="title-sub-section"><div class="title-sub-section-text">{{language.task_szjsnl}}</div><div class="title-sub-section-btn"><el-switch v-model="s.szjs" active-color="#64438D"></el-switch></div></div>
+              <div class="title-sub-section"><div class="title-sub-section-text">{{language.task_scnl}}</div><div class="title-sub-section-btn"><el-switch v-model="s.sc" active-color="#64438D"></el-switch></div></div>
+              <div class="title-sub-section"><div class="title-sub-section-text">{{language.task_dyynl}}</div><div class="title-sub-section-btn"><el-switch v-model="s.dyy" active-color="#64438D"></el-switch></div></div>
+            </div>
           </div>
-        </div>
-        <hr class="draw-hr">
-        <div class="draw-sub">
-          <h1>{{language.Parameter}}</h1>
-          <div>
-            {{drawerInfo.parameter}}
+          <hr />
+          <div class="title-select">
+            <el-checkbox v-model="checked2" disabled>
+              {{language.task_jjb}}
+            </el-checkbox>
           </div>
-        </div>
-        <hr class="draw-hr">
-        <div class="draw-sub">
-          <h1>{{language.More}} {{language.Information}}</h1>
-          <div>
-            {{drawerInfo.more}}
-          </div>
+          <!-- <hr /> -->
         </div>
       </div>
-    </el-drawer>
+      <div class="main-content">
+        <el-table :data="tableData" style="width: 100%" @expand-change="waitToDraw"
+          :header-row-style="{'color': '#ffffff',}"
+          :header-cell-style="{background:'#64438D'}"
+          cell-style="font-weight: 700; color: black;"
+        >
+          <el-table-column label="#" prop="rank"> </el-table-column>
+          <el-table-column label="模型名" prop="name"> </el-table-column>
+          <!-- <el-table-column label="机构" prop="org"> </el-table-column> -->
+          <el-table-column label="论文链接" prop="plink">
+            <template slot-scope="scope">
+              <el-link :href="scope.row.plink">
+                <!-- <i class="el-icon-paperclip"></i> -->
+                <img src="https://z3.ax1x.com/2021/06/24/RQBvFK.png" style="width:30px;height:30px;">
+              </el-link>
+            </template>
+          </el-table-column>
+          <el-table-column label="代码链接" prop="clink">
+            <template slot-scope="scope">
+              <el-link :href="scope.row.clink">
+                <!-- <i class="el-icon-paperclip"></i> -->
+                <img src="https://z3.ax1x.com/2021/06/24/RQBvFK.png" style="width:30px;height:30px;">
+              </el-link>
+            </template>
+          </el-table-column>
+          <el-table-column label="识记能力" prop="sj"> </el-table-column>
+          <el-table-column label="理解能力" prop="lj"> </el-table-column>
+          <el-table-column label="检索能力" prop="js"> </el-table-column>
+          <el-table-column label="数值计算能力" prop="szjs"> </el-table-column>
+          <el-table-column label="生成能力" prop="sc"> </el-table-column>
+          <el-table-column label="多语言能力" prop="dyy"> </el-table-column>
+          <!-- <el-table-column label="总分" prop="score"> </el-table-column> -->
+        </el-table>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
 import en from '@/components/en.json'
+import config from '@/components/config.json'
 export default {
+  name: 'HelloWorld',
   data () {
     return {
       language: en,
-      drawer: false,
-      drawerInfo: {
-        rank: '',
-        name: '',
-        link: '',
-        date: '',
-        score: '',
-        model: '',
-        parameter: '',
-        more: ''
-      },
       tableData: []
-      // tableData: [
-      //   {
-      //     rank: '1',
-      //     date: '19-02-01',
-      //     name: 'The Corpus of Linguistic Acceptability',
-      //     time: '00:30',
-      //     score: '90.5',
-      //     link: 'LINK',
-      //     model: 'xxxxxxxxxxx',
-      //     parameter: 'xxxxxxxxxxxxx',
-      //     more: 'xxxxxxxxxxxxxxx'
-      //   },
-      //   {
-      //     rank: '2',
-      //     date: '19-02-01',
-      //     name: 'The Corpus of Linguistic Acceptability',
-      //     time: '00:30',
-      //     score: '90.5'
-      //   },
-      //   {
-      //     rank: '3',
-      //     date: '19-02-01',
-      //     name: 'The Corpus of Linguistic Acceptability',
-      //     time: '00:30',
-      //     score: '90.5'
-      //   },
-      //   {
-      //     date: '19-02-01',
-      //     name: 'The Corpus of Linguistic Acceptability',
-      //     time: '00:30',
-      //     score: '90.5'
-      //   },
-      //   {
-      //     date: '19-02-01',
-      //     name: 'The Corpus of Linguistic Acceptability',
-      //     time: '00:30',
-      //     score: '90.5'
-      //   },
-      //   {
-      //     date: '19-02-01',
-      //     name: 'The Corpus of Linguistic Acceptability',
-      //     time: '00:30',
-      //     score: '90.5'
-      //   },
-      //   {
-      //     date: '19-02-01',
-      //     name: 'The Corpus of Linguistic Acceptability',
-      //     time: '00:30',
-      //     score: '90.5'
-      //   }
-      // ]
     }
   },
+  mounted: function () {
+    this.tableData = []
+    this.$axios.post(config.API + config.getSubmitlist).then(res => {
+      console.log(res)
+      if (res.status === 200) {
+        console.log(res)
+        var t = res.data.record_list
+        for (var i = 0; i < t.length; i++) {
+          var r = t[i]
+          var toAppend = {
+            rank: i + 1,
+            name: r.模型名,
+            org: 'Test',
+            plink: r.论文链接,
+            clink: r.代码链接,
+            sj: r.识记能力,
+            lj: r.理解能力,
+            js: r.检索能力,
+            szjs: r.数值计算能力,
+            sc: r.生成能力,
+            dyy: r.多语言能力
+          }
+          this.tableData.push(toAppend)
+          console.log(toAppend)
+        }
+      }
+    })
+  },
   methods: {
-    handleRowClick (row, column, event) {
-      this.drawerInfo.rank = row.rank
-      this.drawerInfo.name = row.name
-      this.drawerInfo.link = row.link
-      this.drawerInfo.date = row.date
-      this.drawerInfo.score = row.score
-      this.drawerInfo.model = row.model
-      this.drawerInfo.parameter = row.parameter
-      this.drawerInfo.more = row.more
-      this.drawer = true
-    }
   }
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .all {
-  margin: 100px;
+  width: 90%;
+  margin: auto;
 }
-.table {
-  /* background-color: aqua; */
+.title {
+  text-align: left;
+  margin: 50px;
+  color: #64438d;
+  font-size: 40px;
+}
+.main {
+  margin: 50px;
+}
+.title-title {
+  text-align: left;
+  font-size: x-large;
   font-weight: bold;
 }
-.draw{
+.title-sub-section {
   text-align: left;
-  margin-left: 50px;
-  margin-right: 50px;
 }
-.draw-sub{
-  margin-top: 50px;
+.title-sub-section-text {
+  margin-right: 10px;
+  display: inline;
+  font-size: large;
+  font-weight: bold;
 }
-
-.draw-sub h1{
-  color: #7857A1;
+.title-select {
+  text-align: right;
 }
-.draw-hr{
-  margin-top: 50px;
+.title-select-text {
+  font-size: large;
+  font-weight: bold;
+}
+.title-title,
+.title-sub,
+.title-select {
+  margin-top: 20px;
+}
+.title-sub-section-btn{
+  display: inline;
+}
+.inner-hr{
+  height: 1px;
+  background: #9AB1DE;
+  border: 0;
+}
+.score{
+  font-weight: bolder;
+  color: black;
 }
 </style>
