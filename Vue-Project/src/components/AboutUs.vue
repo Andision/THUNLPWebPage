6d901180ae7a7b5c01d74870111bc1cf35ba40aa
Andision <48558940+Andision@users.noreply.github.com>
2021-09-18 22:55:24 +0800

    Update AboutUs.vue

--- Vue-Project/src/components/AboutUs.vue
+++ Vue-Project/src/components/AboutUs.vue
@@ -5,7 +5,7 @@
         {{language.about_title}}
       </div>
     </div>
-    <el-card style="width:80%; margin: auto; margin-bottom: 50px;">
+    <el-card style="width:90%; margin: auto; margin-bottom: 50px;">
       <div class="content">
         <div class="content-section">
           <!-- <div class="section-title">
@@ -22,7 +22,26 @@
             <!-- <i class="el-icon-tickets"></i> -->
           </div>
           <div class="section-content">
-            <el-image :src="language.about_pic"></el-image>
+            <!-- <el-image :src="language.about_pic"></el-image> -->
+            <el-row :gutter="40">
+              <el-col :span="4.8" v-for="(key,index) in head" :key="index">
+                <el-row class="head-word">
+                  <el-image :src="key.pic"></el-image>
+                </el-row>
+                <el-row class="head-word head-purple" v-if="language.language=='zh'">
+                  {{key.zh.title}}
+                </el-row>
+                <el-row class="head-word" v-if="language.language=='zh'">
+                  {{key.zh.name}}
+                </el-row>
+                <el-row class="head-word head-purple" v-if="language.language=='en'">
+                  {{key.en.title}}
+                </el-row>
+                <el-row class="head-word" v-if="language.language=='en'">
+                  {{key.en.name}}
+                </el-row>
+              </el-col>
+            </el-row>
           </div>
         </div>
 
@@ -63,59 +82,55 @@ export default {
       id: '',
       about_list: {},
       theNull: undefined,
-      show: {
-        title: '',
-        author: [
-          {
-            name: '',
-            link: ''
+      head: [
+        {pic: 'https://z3.ax1x.com/2021/09/18/41UgMV.png',
+          en: {
+            title: 'Director',
+            name: 'Maosing Sun'
           },
-          {
-            name: '',
-            link: ''
-          },
-          {
-            name: '',
-            link: ''
+          zh: {
+            title: '主任',
+            name: '孙茂松'
           }
-        ],
-        org: '',
-        intr: '',
-        paper: '',
-        ref: '',
-        size: '',
-        download: '',
-        form: '',
-        sample: ''
-      },
-      testshow: {
-        title: '金融领域关系抽取数据集FinRE',
-        label: '识记能力 | 开放域问答',
-        hard: '中',
-        reliability: '高',
-        author: [
-          {
-            name: '李自然',
-            link: ''
+        },
+        {pic: 'https://z3.ax1x.com/2021/09/18/41U6x0.png',
+          en: {
+            title: 'Vice Director',
+            name: 'Zhifang Sui'
           },
-          {
-            name: '丁宁',
-            link: ''
+          zh: {
+            title: '副主任',
+            name: '穗志方'
+          }},
+        {pic: 'https://z3.ax1x.com/2021/09/18/41Uy2q.png',
+          en: {
+            title: 'Vice Director',
+            name: 'Erhong Yang'
           },
-          {
-            name: '刘知远',
-            link: ''
-          }
-        ],
-        org: '清华大学',
-        intr: 'FinRE是一个人工精标注的财经金融领域的数据集,该数据集从新浪财经新闻数据中抽取了2647篇文章进行标注,在训练集、验证集和测试集中分别包含了13486, 3727,和1489个句子,为UTF-8纯文本格式。本数据集中的的命名实体为商业公司,在关系上设计了除NA类之外的44个金融领域的关系类别(双向),包含拥有、持股、竞争、收购、交易、合作、、减持等财经金融领域的特有关系类别。',
-        paper: '访问链接阅读论文 https://www.aclweb.org/anthology/P19-1430.pdf',
-        ref: '@inproceedings{li2019chinese, title={Chinese relation extraction withmulti-grained information and external linguistic knowledge), author={Li, Ziranand Ding, Ning and Liu, Zhiyuan and Zheng, Haitao and Shen, Ying},booktitle={Proceedings of the 57th Annual Meeting of the Association forComputational Linguistics}, pages={4377--4386), year={2019}}',
-        size: '训练集7454条句子,验证集1489条句子,测试集3727条句子。',
-        download: '由此链接下载数据集(训练集、验证集、测试集文本)',
-        form: '每条数据包含编号(id),文本(text),关系标签序号(label),关系标签内容(label_desc),头实体(head)以及尾实体(tail)',
-        sample: '{<br>    "id": 2,<br>    "text": "东方航空AH股临时停牌传将与上航合并”,<br>    "label": 19, //若为负例,此处为0<br>    "label_desc": "合并",//若为负例,此处为"N/A"<br>    "head": {<br>        "mention": "东方航空", //实体提及内容<br>        "start": 0, //实体提及在句子中开始的字序号<br>        "end": 4 //实体提及在句子中结束的字序号<br>},<br>    "tail": {<br>        "mention": "EM",<br>        "start": 14,<br>        "end": 16<br>    }<br>}<br>'
-      }
+          zh: {
+            title: '副主任',
+            name: '杨尔弘'
+          }},
+        {pic: 'https://z3.ax1x.com/2021/09/18/41UsGn.png',
+          en: {
+            title: 'Secretary General',
+            name: 'Zhiyuan Liu'
+          },
+          zh: {
+            title: '秘书长',
+            name: '刘知远'
+          }},
+        {pic: 'https://z3.ax1x.com/2021/09/18/41UrPs.png',
+          en: {
+            title: 'Vice Secretary General',
+            name: 'Xianpei Han'
+          },
+          zh: {
+            title: '副秘书长',
+            name: '韩先培'
+          }}
+
+      ]
     }
   },
   methods: {},
@@ -159,19 +174,19 @@ pre{
 <style scoped>
 .all{
   /* font-weight: bold; */
-  margin: auto;
+  margin: 50px;
   width: 90%;
 }
 .title{
-  margin-top: 100px;
+  margin-left: 2%;
   margin-bottom: 100px;
 }
 .title-title{
   color: #64438D;
-  font-size: 60px;
+  font-size: 40px;
   font-weight: 1000;
   text-align: left;
-  margin-left: 200px;
+  margin-left: 50px;
 }
 .title-sub{
   font-size: xx-large;
@@ -214,5 +229,13 @@ pre{
 code {
   white-space: pre-wrap;
 
+}
+.head-word{
+  text-align: center;
+  font-weight: bold;
+  font-size: large;
+}
+.head-purple{
+  color: #64438D;
 }
 </style>
