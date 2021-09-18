c2ad6c0b683d9edf8c76f5e2a452f594c76b99d0
Andision <48558940+Andision@users.noreply.github.com>
2021-09-18 22:16:12 +0800

    Update App.vue

--- Vue-Project/src/App.vue
+++ Vue-Project/src/App.vue
@@ -72,7 +72,7 @@
               <a href=" "  title="Visit tracker"><img src="//www.clustrmaps.com/map_v2.png?d=sRtvKqoXFHRrS9V6nm3IDtLYieMCka7pZnXkwO6ZWyQ&cl=ffffff" /></a>
             </el-col>
             <el-col :span="12" style="padding-top:30px;">
-              <a href="https://github.com/TsinghuaAI/CUGE">https://github.com/TsinghuaAI/CUGE</a>
+              <a href="https://github.com/TsinghuaAI/CUGE" style="font-size:xx-large;">联系我们</a>
             </el-col>
           </el-row>
         </div>
