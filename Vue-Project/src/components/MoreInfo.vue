e3d3fa5040b9d0ef89d73586651f47373dd69c7b
Andision <48558940+Andision@users.noreply.github.com>
2021-10-05 21:51:33 +0800

    Update MoreInfo.vue

--- Vue-Project/src/components/MoreInfo.vue
+++ Vue-Project/src/components/MoreInfo.vue
@@ -230,7 +230,7 @@ export default {
 pre{
   white-space: normal;
   /* font-weight: bold; */
-  font-family: -apple-system,BlinkMacSystemFont,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Segoe UI","PingFang SC","Hiragino Sans GB","Microsoft YaHei","Helvetica Neue",Helvetica,Arial,sans-serif;
+  font-family: "Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Segoe UI","PingFang SC","Hiragino Sans GB","Microsoft YaHei","Helvetica Neue",Helvetica,Arial,sans-serif;
 }
 .bg-color{
   background-color: #eceaff;
