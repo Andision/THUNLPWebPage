c6779698e30df517a693084da2c52b4958f04b5d
Andision <48558940+Andision@users.noreply.github.com>
2021-10-05 21:06:27 +0800

    Update Home.vue

--- Vue-Project/src/components/Home.vue
+++ Vue-Project/src/components/Home.vue
@@ -5,13 +5,13 @@
     > -->
     <div class="home-title">
       <div class="title1">
-        {{language.home_title1}}
+        {{ language.home_title1 }}
       </div>
       <div class="title2">
-        {{language.home_title2}}
+        {{ language.home_title2 }}
       </div>
       <div class="titledetail">
-        {{language.home_titledetail}}
+        {{ language.home_titledetail }}
       </div>
     </div>
 
@@ -20,8 +20,8 @@
         <!-- {{language.home_secondtitle}} -->
         委员单位 | Committee Organization
       </div>
-      <hr class="second-hr">
-      <div class="second-list" style="min-height:300px;">
+      <hr class="second-hr" />
+      <!-- <div class="second-list" style="min-height:300px;">
         <ul>
           <li class="second-list-item" v-for="(item,index) in org" :key="index">
             ●{{kb}} {{kb}}{{ item.zh }}
@@ -30,30 +30,43 @@
             <br>
           </li>
         </ul>
-      </div>
-      <!-- <div v-for="(row,index1) in neworg" :key="index1">
-        <el-row>
-          <el-col :span="6" v-for="(col,index2) in row" :key="index2">
-            <el-image :src="col" style="width:200px; height:60px;background-color:white;"></el-image>
-          </el-col>
-        </el-row>
-        <hr class="second-hr" color="#F5F4FE" />
       </div> -->
-      <hr class="second-hr" color="#F5F4FE" />
-      <div class="second-logo">
-        <div class="second-logo-item" v-for="(item,index) in logo" :key="index">
-          <img class="pic-logo" :src="item"/>
+      <div style="margin: 50px;">
+        <div v-for="(row, index1) in neworg" :key="index1" style="margin-top: 30px;">
+          <el-row>
+            <el-col :span="6" v-for="(col, index2) in row" :key="index2">
+              <el-row>
+                <el-image
+                  :src="col.pic"
+                  style=" height: 100px;"
+                ></el-image>
+              </el-row>
+              <el-row> {{col.zh}} </el-row>
+              <el-row> {{col.en}} </el-row>
+            </el-col>
+          </el-row>
+          <hr class="second-hr" color="#F5F4FE" />
         </div>
       </div>
+      <hr class="second-hr" color="#F5F4FE" />
+      <!-- <div class="second-logo">
+        <div
+          class="second-logo-item"
+          v-for="(item, index) in logo"
+          :key="index"
+        >
+          <img class="pic-logo" :src="item" />
+        </div>
+      </div> -->
     </div>
 
-    <div class="home-second" style="margin-top:-50px;">
+    <div class="home-second" style="margin-top: -50px">
       <div class="second-title">
         <!-- {{language.home_secondtitle}} -->
         支持单位 | Supporting Organization
       </div>
-      <hr class="second-hr">
-      <div class="second-list" style="text-align:center;margin-top:30px;">
+      <hr class="second-hr" />
+      <div class="second-list" style="text-align: center; margin-top: 30px">
         <!-- <div style="margin:auto;" v-for="(item,index) in org1" :key="index">
           <el-row>
             <el-col :span="8" style="min-height:10px;">
@@ -74,11 +87,13 @@
             </el-col>
           </el-row>
         </div> -->
-
       </div>
       <hr class="second-hr" color="#F5F4FE" />
       <a href="https://www.baai.ac.cn/">
-        <el-image src="https://z3.ax1x.com/2021/09/16/4nzcTS.jpg" style="width:200px; height:80px;"></el-image>
+        <el-image
+          src="https://z3.ax1x.com/2021/09/16/4nzcTS.jpg"
+          style="width: 200px; height: 80px"
+        ></el-image>
       </a>
       <!-- <div class="second-logo" style="text-align:center;">
         <el-row>
@@ -174,21 +189,25 @@ export default {
         'https://z3.ax1x.com/2021/06/02/2lNlEq.png',
         'https://z3.ax1x.com/2021/06/02/2lNMbn.png'
       ],
-      logo1: [
-        'https://z3.ax1x.com/2021/09/07/ho7fHA.jpg'
-      ],
+      logo1: ['https://z3.ax1x.com/2021/09/07/ho7fHA.jpg'],
       neworg: [
-        ['https://z3.ax1x.com/2021/09/19/4310Bj.png',
-          'https://z3.ax1x.com/2021/09/19/431UgS.png',
-          'https://z3.ax1x.com/2021/09/19/431N38.jpg',
-          'https://z3.ax1x.com/2021/09/19/431ajg.png'],
-        ['https://z3.ax1x.com/2021/09/19/431wuQ.png',
-          'https://z3.ax1x.com/2021/09/19/431BHs.png',
-          'https://z3.ax1x.com/2021/09/19/431rEn.png',
-          'https://z3.ax1x.com/2021/09/19/431sNq.png'],
-        ['https://z3.ax1x.com/2021/09/19/431y40.png',
-          'https://z3.ax1x.com/2021/09/19/431cCV.jpg',
-          'https://z3.ax1x.com/2021/09/19/431g3T.jpg']
+        [
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vDfj1.png', zh: '清华大学', en: 'Tsinghua University' },
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vDyAU.png', zh: '北京大学', en: 'Peking University' },
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vD29J.png', zh: '中国科学院', en: 'Chinese Academy of Sciences' },
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vD6NF.png', zh: '中国人民大学', en: 'Renmin University' }
+        ],
+        [
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vDr7T.png', zh: '北京语言大学', en: 'Beijing Language and Culture University' },
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vDIHK.png', zh: '山西大学', en: 'Shanxi University' },
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vDR39.png', zh: '大连理工大学', en: 'Dalian University of Technology' },
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vDch4.png', zh: '哈尔滨工业大学', en: 'Harbin Institute of Technology' }
+        ],
+        [
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vD4nx.png', zh: '复旦大学', en: 'Fudan University' },
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vDWcR.png', zh: '苏州大学', en: 'Soochow University' },
+          { pic: 'https://z3.ax1x.com/2021/10/05/4vD5B6.png', zh: '京东', en: 'JD.COM' }
+        ]
       ]
       // neworg: [
       //   ['https://z3.ax1x.com/2021/09/18/41kB4A.png',
@@ -213,45 +232,45 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.el-main{
-  background-color: #F5F4FE;
+.el-main {
+  background-color: #f5f4fe;
 }
-.main{
-  background-color: #F5F4FE;
+.main {
+  background-color: #f5f4fe;
 }
 </style>
 
 <style scoped>
-.all{
-  background-color: #F5F4FE;
+.all {
+  background-color: #f5f4fe;
   min-height: 800px;
 }
-.home-title{
+.home-title {
   text-align: center;
   background-color: white;
   padding: 50px;
   padding-top: 200px;
   padding-bottom: 200px;
 }
-.home-second{
+.home-second {
   padding: 50px;
   text-align: center;
 }
-.title1{
-  color: #7857A1;
+.title1 {
+  color: #7857a1;
   font-size: 70px;
   font-weight: bold;
   margin: 20px;
 }
-.title2{
-  color: #7857A1;
+.title2 {
+  color: #7857a1;
   font-weight: bold;
   margin: 20px;
   font-size: 40px;
   padding: 50px;
   margin-top: -50px;
 }
-.titledetail{
+.titledetail {
   font-weight: normal;
   padding-left: 10%;
   padding-right: 10%;
@@ -259,18 +278,18 @@ export default {
   text-align: left;
   font-size: 30px;
   margin-top: -50px;
-  text-align:justify
+  text-align: justify;
 }
-.second-title{
-  color: #062A4E;
+.second-title {
+  color: #062a4e;
   font-size: larger;
   font-weight: bold;
 }
-.second-hr{
+.second-hr {
   width: 90%;
 }
 
-.second-list{
+.second-list {
   margin-left: 10%;
 }
 
@@ -282,46 +301,44 @@ export default {
   list-style-type: none;
   text-align: left;
 }
-.second-list-item{
-  color: #062A4E;
+.second-list-item {
+  color: #062a4e;
   font-size: medium;
   font-weight: bold;
   min-height: 50px;
 }
 
-.second-logo{
+.second-logo {
   margin-left: 100px;
   margin-right: 100px;
   margin-top: 30px;
   min-height: 100px;
 }
-.second-logo-item{
+.second-logo-item {
   list-style-type: none;
-  float:left;
+  float: left;
   margin-left: 50px;
   margin-bottom: 10px;
 }
-.pic-logo{
+.pic-logo {
   width: 100px;
   height: 40px;
   display: flex;
-
 }
-.pic-logo-support{
+.pic-logo-support {
   width: 150px;
   height: 40px;
   /* display: flex; */
-
 }
-.second-logo-item-support{
+.second-logo-item-support {
   /* list-style-type: none;
   float:left;
   margin-bottom: 10px; */
   /* margin-left: 800px; */
   text-align: center;
 }
-.second-list-item-support{
-  color: #062A4E;
+.second-list-item-support {
+  color: #062a4e;
   font-size: medium;
   font-weight: bold;
   /* margin-left: 100px; */
